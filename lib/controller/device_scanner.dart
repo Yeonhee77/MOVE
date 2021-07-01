b4495e88250aa31df61176f812f8ec518ec33fbb
jonghyun <jonghyun@jonghyunui-MacBookPro.local>
2021-07-01 17:08:31 +0900

    JH/0701/ble scan code modified - device.name.toString -> contains()

--- lib/controller/device_scanner.dart
+++ lib/controller/device_scanner.dart
@@ -25,7 +25,7 @@ class DeviceScanner {
   void _subscribeToScanEvents() {
     FlutterBlue.instance.scanResults.listen((scanResults) {
       for (ScanResult scanResult in scanResults) {
-        if (scanResult.device.name.toString() == "Move! - 2405") {
+        if (scanResult.device.name.contains("Move")) {
           print('Device : ' + scanResult.device.name.toString());
           print('Bluetooth found');
           final double result_value = scanResult.advertisementData.manufacturerData[256]![0]*1.00;
