0ff2da0a0600d260ca506418c05111f9c8e5a78d
YJ K <yjk@YJui-MacBookPro.local>
2021-07-02 16:02:50 +0900

    YJ/0702/revised

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MOVE!',
+      title: 'MOVE!',//Move
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
