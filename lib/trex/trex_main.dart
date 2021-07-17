59b79e588ea229cc333241290b8a4c1a87f79bcc
jonghyun <jonghyun@jonghyunui-MacBookPro.local>
2021-07-17 18:52:08 +0900

    JH/0717/dino game modified

--- lib/trex/trex_main.dart
+++ lib/trex/trex_main.dart
@@ -123,7 +123,6 @@ class _TRexGameWrapperState extends State<TRexGameWrapper> {
   }
 
   Widget scoreBox(BuildContext buildContext, TRexGame game) {
-    bool visible = true;
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
         children: [
